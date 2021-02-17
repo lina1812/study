866a91ae67a811799ea801c68e2974b6f10ea388
Lina1812 <alena18.12.1992@mail.ru>
2021-02-18 00:10:01 +0300

    changes after review

--- railway/validation.rb
+++ railway/validation.rb
@@ -4,24 +4,34 @@ module Validation
   def self.included(base)
     base.extend ClassMethods
     base.include InstanceMethods
-    base.class_variable_set('@@validation_rules', [])
+    base.class_variable_set('@@validations', [])
   end
 
   module ClassMethods
     def validate(name, type, rule = nil)
       var_name = "@#{name}".to_sym
-      class_variable_get('@@validation_rules') << [var_name, type, rule]
+      class_variable_get('@@validations') << { attr: var_name, type: type, options: rule }
     end
   end
 
   module InstanceMethods
     def validate!
-      self.class.class_variable_get('@@validation_rules').each do |rule|
-        raise "#{rule[0]} can't be nil" if rule[1] == :presence && !instance_variable_defined?(rule[0])
-        raise "#{rule[0]} has invalid format" if rule[1] == :format && instance_variable_get(rule[0]) !~ rule[2]
-        raise "#{rule[0]} should be a #{rule[3]}" if rule[1] == :type && instance_variable_get(rule[0]).is_a?(rule[3])
+      self.class.class_variable_get('@@validations').each do |rule|
+        send("validate_#{rule[:type]}", **rule)
       end
       true
+    end
+
+    def validate_presence(attr:, **)
+      raise "#{attr} can't be nil" if !instance_variable_defined?(attr) && instance_variable_get(attr) != ''
+    end
+
+    def validate_format(attr:, options:, **)
+      raise "#{attr} has invalid format" if instance_variable_get(attr) !~ options
+    end
+
+    def validate_type(attr:, options:, **)
+      raise "#{attr} should be a #{options}" if instance_variable_get(attr).is_a?(options)
     end
 
     def valid?
